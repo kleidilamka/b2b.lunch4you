8845e029471318f11f8344edd48a76b27d1e9b94
kleidilamka <kleidilamka@gmail.com>
2022-05-30 13:55:30 +0200

    Footer finished

--- resources/js/components/Footer/Footer.js
+++ resources/js/components/Footer/Footer.js
@@ -7,24 +7,37 @@ const Footer = () => {
     return (
         <div class={styles.root}>
             <div class={styles.subscribeContainer}>
-                <h2 style={{ color: "#bd006c" }}>LECKER NEWSELLTER.</h2>
-                <h1 class={styles.title}>
-                    JETZT FÜR UNSEREN NEWSLETTER ANMELDEN UND GRATISVERSAND
-                    SICHERN!
+                <h2
+                    class={`${styles.title} ${styles.font}`}
+                    style={{ color: "#bd006c" }}
+                >
+                    LECKER NEWSELLTER.
+                </h2>
+                <h1 class={`${styles.bigTitle} ${styles.font}`}>
+                    JETZT FÜR UNSEREN NEWSLETTER ANMELDEN <br />
+                    UND GRATISVERSAND SICHERN!
                     <div class={styles.inputs}>
                         <input
                             class={styles.input}
                             placeholder="E-Mail eingeben"
                         />
-                        <button class={styles.button}>
+                        <button class={`${styles.button} ${styles.font}`}>
                             NEWSLETTER BESTELLEN
                         </button>
                     </div>
                 </h1>
             </div>
             <div class={styles.topContainer}>
-                <FaInstagram size={30} color={"white"} />
-                <FaFacebookSquare size={30} color={"white"} />
+                <FaInstagram
+                    style={{ paddingLeft: 50 }}
+                    size={30}
+                    color={"white"}
+                />
+                <FaFacebookSquare
+                    size={30}
+                    color={"white"}
+                    style={{ marginLeft: 20 }}
+                />
             </div>
             <div class={styles.middleContainer}>
                 <div class={styles.middleLeftContainer}>
@@ -48,7 +61,9 @@ const Footer = () => {
                 </div>
             </div>
             <div class={styles.bottomContainer}>
-                <h4>© 2022 Creativ-Cult All rights reserved.</h4>
+                <h4 style={{ paddingLeft: 50 }}>
+                    © 2022 Creativ-Cult All rights reserved.
+                </h4>
             </div>
         </div>
     );
